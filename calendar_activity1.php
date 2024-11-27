dc860b0d5f1a46fee1b949feedce39b73c6850a3
Danilo Medallo Jr. <108509327+DanFreeCoder@users.noreply.github.com>
2024-11-27 14:16:19 +0800

    Update calendar_activity1.php

--- calendar_activity1.php
+++ calendar_activity1.php
@@ -1,27 +1,24 @@
 <?php
 function get_calendar_by_year($year)
 {
-    $num_of_month = [0, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    $num_of_month = [1,'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     foreach ($num_of_month as $num => $string) {
         if ($num > 0) {
-            echo "<h1>$string</h1>" . "<br>";
+            echo "<h1>$string</h1>";
             $number_of_days = cal_days_in_month(CAL_GREGORIAN, $num, $year);
+            echo "<ul>";
             for ($day = 1; $day <= $number_of_days; $day++) {
-                $dayString = date('l', strtotime("$year-$num-$day")) . '<br>';
-                $date = date('d', strtotime("$year-$num-$day")) . '<br>';
+                $dayString = date('l', strtotime("$year-$num-$day"));
+                $date = date('d', strtotime("$year-$num-$day"));
                 echo "
-                <table class='table'>
-                <thead>
-                <th>$dayString</th>
-                 </thead>
-                    <tbody>
-                        <tr>
-                            <td>$date</td>
-                        </tr>
-                    </tbody>
-                    </table>
+                        
+                        <div class='list'>
+                        <p class='text-center'><strong>".$dayString."</strong></p>
+                        <li class='text-center'><time datetime=''>".$date."</time></li>
+                        </div>
                 ";
             }
+            echo "</ul>";
         }
     }
 }
@@ -34,21 +31,27 @@ function get_calendar_by_year($year)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.scss">
 </head>
 
 <body>
-    <form action="index.php" method="post">
-        <input type="text" placeholder="Enter a year" name="year">
+    <form action="calendar_activity1.php" method="post" class="d-flex gap-2 mt-3">
+        <input type="text" placeholder="Enter a year" class="form-control" name="year">
         <button type="submit" class="btn btn-success btn-sm">Submit</button>
     </form>
     <?php
     if (isset($_POST['year'])) {
         echo "<h1>Year of " . $_POST['year'] . "</h1><br>";
+        
         get_calendar_by_year($_POST['year']);
     } else {
-        echo 'Enter Year';
+        echo "<h1>Year of " . date('Y') . "</h1><br>";
+        
+        get_calendar_by_year(date('Y'));
     }
     ?>
+
+
 </body>
 
 </html>
